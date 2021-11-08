5f49069d55ff90be418f4a6ad651b9c5fb4ddcd9
Shah619 <shah.iqram@sky.uk>
2021-11-08 14:25:16 +0000

    fibonacci

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from saif import saif
 from shah import shah
 from jonathan import jonathan
+from fibonacci import fibonacci
 
 def main():
 
-    saif()
+    # saif()
 
-    shah()
+    # shah()
 
-    jonathan()
+    # jonathan()
+
+    inputNum = int(input("Input a number: "))
+    print(f'The input number is {inputNum} and the fibonacci number is {fibonacci(inputNum)}')
 
 if __name__== "__main__":
     main()
